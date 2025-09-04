797a0691881af73ba309c19bba4d5336f4fff04e
Nathan <nathan.eleveld@student.nhlstenden.com>
2025-09-04 17:51:02 +0200

    Moved some things

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dikke index pagina</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<header>
+    <H1>Dit is een dikke index pagina.</H1>
+</header>
+<body>
+    <?php
+        $map = "Opdrachten";
+        $bestanden = scandir($map);
+
+        foreach ($bestanden as $bestand) {
+            if ($bestand !== "." && $bestand !== "..") {
+                echo $bestand . "<br>";
+            }
+        }
+    ?>
+</body>
+</html>
