20bd21c7e6938cbb4b67c44e925dbdb03a4d5f5c
Nathan <nathan.eleveld@student.nhlstenden.com>
2025-09-04 09:09:27 +0200

    Added a index page.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dikke index pagina</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Dit is een dikke index pagina.</h1>
+</body>
+</html>
