cc7bc8c7cbdbff4057248e2203e46ae63aaeeaee
Nathan <nathan.eleveld@student.nhlstenden.com>
2025-09-09 18:32:50 +0200

    Added better structure

--- index.php
+++ index.php
@@ -11,13 +11,13 @@
 </header>
 <body>
     <h2>Wekelijkse opdrachten</h2>
-    <a href="Opdrachten\Leerjaar 1\Periode 1\Programmeren\Opdrachten Week 1\Voorbeeld 1\amsterdam_info.html">Opdracht 1</a><br>
-    <a href="Opdrachten\Leerjaar 1\Periode 1\Programmeren\Opdrachten Week 1\Voorbeeld 2\amsterdam_info.html">Opdracht 2</a><br>
-    <a href="Opdrachten\Leerjaar 1\Periode 1\Programmeren\Opdrachten Week 1\Voorbeeld 3\amsterdam_info.html">Opdracht 3</a><br>
-    <a href="Opdrachten\Leerjaar 1\Periode 1\Programmeren\Opdrachten Week 1\Voorbeeld 4\overview_bridges.html">Opdracht 4</a><br>
-    <a href="Opdrachten\Leerjaar 1\Periode 1\Programmeren\Opdrachten Week 1\Voorbeeld 5-Opdracht Van Winteren\text.php">Opdracht 5</a><br>
-    <a href="Opdrachten\Leerjaar 1\Periode 1\Programmeren\Opdrachten Week 1\Voorbeeld 6\opdracht.php">Opdracht 6</a><br>
-    <a href="Opdrachten\Leerjaar 1\Periode 1\Programmeren\Opdrachten Week 1\Voorbeeld 7\opdracht.php">Opdracht 7</a><br>
-    <a href="Opdrachten\Leerjaar 1\Periode 1\Programmeren\Opdrachten Week 1\Voorbeeld 8\opdracht.php">Opdracht 8</a><br>
+    <a href="Opdrachten\Leerjaar_1\Periode_1\Programmeren\Opdrachten_Week_1\Opdracht_1\amsterdam_info.html">Opdracht 1</a><br>
+    <a href="Opdrachten\Leerjaar_1\Periode_1\Programmeren\Opdrachten_Week_1\Opdracht_2\amsterdam_info.html">Opdracht 2</a><br>
+    <a href="Opdrachten\Leerjaar_1\Periode_1\Programmeren\Opdrachten_Week_1\Opdracht_3\amsterdam_info.html">Opdracht 3</a><br>
+    <a href="Opdrachten\Leerjaar_1\Periode_1\Programmeren\Opdrachten_Week_1\Opdracht_4\overview_bridges.html">Opdracht 4</a><br>
+    <a href="Opdrachten\Leerjaar_1\Periode_1\Programmeren\Opdrachten_Week_1\Opdracht_5\text.php">Opdracht 5</a><br>
+    <a href="Opdrachten\Leerjaar_1\Periode_1\Programmeren\Opdrachten_Week_1\Opdracht_6\opdracht.php">Opdracht 6</a><br>
+    <a href="Opdrachten\Leerjaar_1\Periode_1\Programmeren\Opdrachten_Week_1\Opdracht_7\opdracht.php">Opdracht 7</a><br>
+    <a href="Opdrachten\Leerjaar_1\Periode_1\Programmeren\Opdrachten_Week_1\Opdracht_8\opdracht.php">Opdracht 8</a><br>
 </body>
 </html>
