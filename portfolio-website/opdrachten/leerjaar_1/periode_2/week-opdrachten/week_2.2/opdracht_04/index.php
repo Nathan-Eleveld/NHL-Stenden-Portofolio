fa035e6c836fd9520afdfec5559d88292bb109a2
Nathan <nathan.eleveld@student.nhlstenden.com>
2025-12-17 22:24:44 +0100

    added get file function

--- portfolio-website/opdrachten/leerjaar_1/periode_2/week-opdrachten/week_2.2/opdracht_04/index.php
+++ portfolio-website/opdrachten/leerjaar_1/periode_2/week-opdrachten/week_2.2/opdracht_04/index.php
@@ -14,11 +14,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document Overzicht</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <div class="card-overview">
         <?php
-            
+            $fileLoc = '../opdracht_03/upload/';
+
+            function getFiles($fileLoc){
+                $files = scandir($fileLoc);
+                foreach($files as $file){
+                    if($file === '.' || $file === '..'){
+                        continue;
+                    }
+                    $path = $fileLoc . $file;
+
+                    echo "<div class='card'>";
+                    echo "Naam: $file<br>";
+                    echo "Type: " . mime_content_type($path) . "<br>";
+                    echo "Grootte: " . filesize($path) . " bytes<br>";
+                    echo "Laatst gewijzigd: " . date("d-m-Y H:i:s", filemtime($path)) . "<br><br>";
+                    echo "</div>";
+                }
+            }
+
+            getFiles($fileLoc);
         ?>
     </div>
 </body>
