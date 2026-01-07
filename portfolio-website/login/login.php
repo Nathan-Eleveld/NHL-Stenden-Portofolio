6acaefa6b9725d442687aedbadc3641011a2d9fb
Nathan <nathan.eleveld@student.nhlstenden.com>
2026-01-07 21:40:25 +0100

    added bugreporter and started with new index/portfolio

---
+++ portfolio-website/login/login.php
@@ -0,0 +1,29 @@
+<?php
+    $isLoggedIn = false;
+    $userNameInput = null;
+    $userPassInput = null;
+
+    function DatabaseConnect(){
+
+    }
+
+    function CreateUser(){
+
+    }
+
+    function GetUserById(){
+
+    }
+
+    function GetAllUser(){
+        
+    }
+
+    function UpdateUser(){
+
+    }
+
+    function DeleteUser(){
+
+    }
+?>
---
+++ portfolio-website/opdrachten/leerjaar_1/periode_2/week-opdrachten/bugreporter/index.php
@@ -0,0 +1,93 @@
+<?php
+  $ex = null;
+  $dbConnection = null;
+
+  function connectDB($dbConnection){
+    try{
+      $dbConnection = new PDO("mysql:host=mysql;dbname=bugreporter;charset=utf8", "root", "qwerty");
+      return $dbConnection;
+    }catch(Exception $ex){
+      return $ex;
+    }
+  }
+
+  function getAllBugs($dbConnection){
+    $dbConnection = connectDB($dbConnection);
+    if($dbConnection){
+      try{
+        $stmt = $dbConnection->prepare(
+          "
+            SELECT *
+            FROM `bugs`
+          "
+        );
+        $stmt->execute();
+
+        try{
+          if(isset($stmt)){
+            $stmt->bindColumn("id", $id);
+            $stmt->bindColumn("productname", $productName);
+            $stmt->bindColumn("version", $version);
+            $stmt->bindColumn("hardwaretype", $hardwareType);
+            $stmt->bindColumn("os", $OS);
+            $stmt->bindColumn("frequency", $frequency);
+            $stmt->bindColumn("solution", $solution);
+
+            $stmt->execute();
+          }
+        }catch(Exception $ex){
+          return $ex;
+        }
+
+        while($result = $stmt->fetch()){
+          echo '
+                <tr>
+                  <td>' . $productName . '</td>
+                  <td>' . $version . '</td>
+                  <td>' . $hardwareType . '</td>
+                  <td>' . $OS . '</td>
+                  <td>' . $frequency . '</td>
+                  <td>' . $solution . '</td>
+                  <td><a href="Edit link">Edit</a></td>
+                </tr>
+          ';
+        }
+        $stmt->closeCursor();
+      }catch(Exception $ex){
+        return $ex;
+      }
+    }
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bugreporter</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <table>
+      <tr>
+        <th>Product Name</th>
+        <th>Version</th>
+        <th>Hardware Type</th>
+        <th>OS</th>
+        <th>Frequency</th>
+        <th>Solution</th>
+        <th>Edit link</th>
+      </tr>
+      <?php
+        if($ex === null){
+          getAllBugs($dbConnection);
+        }else{
+          echo "Hij dut nie:";
+          echo $ex;
+        };
+      ?>
+</table>
+
+</body>
+</html>
